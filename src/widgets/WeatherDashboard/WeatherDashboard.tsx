3a4a214dbba892df0a6024aa18bc82e784882030
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-09-01 21:30:34 +0200

    feat: implement search tips (arrows)

--- src/widgets/WeatherDashboard/WeatherDashboard.tsx
+++ src/widgets/WeatherDashboard/WeatherDashboard.tsx
@@ -1,5 +1,6 @@
 import { useSelector } from 'react-redux';
-import { FaSearchLocation } from 'react-icons/fa';
+import { TbLocation } from 'react-icons/tb';
+import { FaLongArrowAltUp } from 'react-icons/fa';
 import { RootState } from '../../redux/store';
 import styles from './WeatherDashboard.module.scss';
 import LocationSearch from './LocationSearch/LocationSearch';
@@ -7,6 +8,8 @@ import useGeolocationPosition from '../../hooks/useNavigationLocation';
 import Button from '../../components/Button/Button';
 import CurrentWeather from './CurrentWeather/CurrentWeather';
 import Forecast from './Forecast/Forecast';
+
+const ICONS_SIZE = '2rem';
 
 const WeatherDashboard = () => {
   const { currentWeather } = useSelector((state: RootState) => state.weather);
@@ -19,7 +22,7 @@ const WeatherDashboard = () => {
         <LocationSearch />
         <Button variant='text' onClick={getCurrentPosition}>
           <p className={styles['location-button']}>
-            Locate me <FaSearchLocation />
+            <TbLocation size={ICONS_SIZE} />
           </p>
         </Button>
       </div>
@@ -29,10 +32,17 @@ const WeatherDashboard = () => {
           <Forecast />
         </div>
       ) : (
-        <p>
-          Enter a city name or confirm your location to get an accurate weather forecast. Don`t know
-          where it is? Find a place based on your current location!
-        </p>
+        <div className={styles.welcome}>
+          <p className={styles.arrows}>
+            <FaLongArrowAltUp className={styles.red} size={ICONS_SIZE} />
+            <FaLongArrowAltUp className={styles.green} size={ICONS_SIZE} />
+          </p>
+          <p className={styles['welcome-message']}>
+            <span className={styles.red}>Enter a city name </span> or{' '}
+            <span className={styles.green}>confirm your location</span> to get an accurate weather
+            forecast. Don`t know where it is? Find a place based on your current location!
+          </p>
+        </div>
       )}
     </div>
   );
