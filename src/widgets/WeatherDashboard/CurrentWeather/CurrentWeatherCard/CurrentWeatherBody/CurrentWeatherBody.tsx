27494161a60db05e28ac027d59357c0e9ba0938f
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-09-02 10:19:01 +0200

    refactor: format code;

--- src/widgets/WeatherDashboard/CurrentWeather/CurrentWeatherCard/CurrentWeatherBody/CurrentWeatherBody.tsx
+++ src/widgets/WeatherDashboard/CurrentWeather/CurrentWeatherCard/CurrentWeatherBody/CurrentWeatherBody.tsx
@@ -1,26 +1,25 @@
 import { FaWind, FaWater } from 'react-icons/fa';
 import { WiBarometer } from 'react-icons/wi';
 import { CurrentWeatherBodyProps } from '../CurrentWeatherCard.types';
+import { LARGE_ICON_SIZE } from '../../../../../constants';
 import styles from './CurrentWeatherBody.module.scss';
-
-const ICON_SIZE = '3rem';
 
 const CurrentWeatherBody = ({ weather }: CurrentWeatherBodyProps) => {
   const bodyList = [
     {
       name: 'Humidity',
       value: `${weather.main.humidity}%`,
-      icon: <FaWater size={ICON_SIZE} />,
+      icon: <FaWater size={LARGE_ICON_SIZE} />,
     },
     {
       name: 'Wind Speed',
       value: `${weather.wind.speed} m/s`,
-      icon: <FaWind size={ICON_SIZE} />,
+      icon: <FaWind size={LARGE_ICON_SIZE} />,
     },
     {
       name: 'Pressure',
       value: `${weather.main.pressure} hPa`,
-      icon: <WiBarometer size={ICON_SIZE} />,
+      icon: <WiBarometer size={LARGE_ICON_SIZE} />,
     },
   ];
 
