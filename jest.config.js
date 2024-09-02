cd4d130d040c26e0f732f8e18216f3add6239619
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-09-02 08:30:16 +0200

    chore: add testing tools and configure jest

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/build/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
