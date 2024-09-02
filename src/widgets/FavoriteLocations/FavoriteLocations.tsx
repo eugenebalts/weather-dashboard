a70ebc30bf4b64ca482ea218b170b67557f5d051
Yauheni Baltsevich <norcobeatz@gmail.com>
2024-09-02 02:56:02 +0200

    feat: render message if No fav locations

--- src/widgets/FavoriteLocations/FavoriteLocations.tsx
+++ src/widgets/FavoriteLocations/FavoriteLocations.tsx
@@ -10,13 +10,17 @@ const FavoriteLocations = () => {
   return (
     <Section width='250px'>
       <h2>Favorite Locations</h2>
-      <ul className={styles.list}>
-        {favoriteLocations.map((location) => (
-          <li className={styles.item} key={location.lat}>
-            <FavoriteLocationCard coordinatesWithMetadata={location} />
-          </li>
-        ))}
-      </ul>
+      {favoriteLocations.length ? (
+        <ul className={styles.list}>
+          {favoriteLocations.map((location) => (
+            <li className={styles.item} key={location.lat}>
+              <FavoriteLocationCard coordinatesWithMetadata={location} />
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={styles.message}>No Content</p>
+      )}
     </Section>
   );
 };
